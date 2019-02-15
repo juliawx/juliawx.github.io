4489d0569a772fd360e1f1a7acfee10f33e02198
juliawxu <julia.xu.44@gmail.com>
2019-02-14 22:53:48 -0600

    Updates

---
+++ precache-manifest.9010398100e10a3a4bc5b3f2edc9d5f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "40b5f4f88b444593f8dcf53b2042c991",
+    "url": "/juliawxu.github.io/static/media/about3.40b5f4f8.png"
+  },
+  {
+    "revision": "e11a8f75f2e9c4b0b17a",
+    "url": "/juliawxu.github.io/static/js/runtime~main.e11a8f75.js"
+  },
+  {
+    "revision": "608f5cc963f6120e3704",
+    "url": "/juliawxu.github.io/static/js/main.608f5cc9.chunk.js"
+  },
+  {
+    "revision": "fad89c34c085cefbdb8a",
+    "url": "/juliawxu.github.io/static/js/2.fad89c34.chunk.js"
+  },
+  {
+    "revision": "608f5cc963f6120e3704",
+    "url": "/juliawxu.github.io/static/css/main.8e56fd4d.chunk.css"
+  },
+  {
+    "revision": "f59e450743b64322191ab299471e8e8d",
+    "url": "/juliawxu.github.io/index.html"
+  }
+];
